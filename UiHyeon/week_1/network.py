a45207f98fe0b08425e339986268a58144cb2f7b
UiHyeon <yh4268@koreatech.ac.kr>
2021-06-10 21:50:26 +0900

    refactor: 피드백 수정

--- UiHyeon/week_1/network.py
+++ UiHyeon/week_1/network.py
@@ -1,10 +1,25 @@
 NET_PLUS = 1
 CONNECT = 1
 
+def count_net(graph, n):
+    visit = [0 for i in range(len(graph))]
+    net_num = 0
+                
+    for base_com in range(n):
+        stack = [base_com]
+        if visit[base_com] != 1:
+            net_num += NET_PLUS
+        
+        while stack:
+            node = stack.pop()
+            if visit[node] != 1:
+                visit[node] = 1
+                stack.extend(graph[node])
+                
+    return net_num
+
 def solution(n, computers):
-    answer = 0
     graph = {}
-    visit = []
     
     for com_num in range(n):
         graph[com_num] = []
@@ -13,18 +28,5 @@ def solution(n, computers):
         for j in range(len(computers[0])):
             if computers[i][j] == CONNECT:
                 graph[i].append(j)
-                
-    for base_com in range(n):
-        stack = [base_com]
-
-        if base_com not in visit:
-            answer += NET_PLUS
         
-        while stack:
-            node = stack.pop()
-
-            if node not in visit:
-                visit.append(node)
-                stack.extend(graph[node])
-        
-    return answer
+    return count_net(graph, n)
