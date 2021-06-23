264517b5adc09329dc2fe425c2ba2b6634518b88
UiHyeon <yh4268@koreatech.ac.kr>
2021-06-23 14:45:25 +0900

    refactor: change 'slave' to except_num

--- UiHyeon/week_3/array_turn.py
+++ UiHyeon/week_3/array_turn.py
@@ -7,7 +7,7 @@ def solution(rows, columns, queries):
     for i in range(len(queries)):
         base_r, base_c = queries[i][0], queries[i][1]
         end_r, end_c = queries[i][2], queries[i][3]
-        slave = graph[base_r-1][end_c-1]
+        except_num = graph[base_r-1][end_c-1]
         mini = slave
         #상
         for j in range(end_c-1, base_c-1, -1): 
@@ -30,7 +30,7 @@ def solution(rows, columns, queries):
             graph[j][end_c-1] = temp
             mini = min(temp, mini)
             
-        graph[base_r][end_c-1] = slave
+        graph[base_r][end_c-1] = except_num
         answer.append(mini)
             
     return answer
